0673f048561c1394bc2364898dd0d3c828ae9c9c
Kami <kamdin@web.de>
2024-11-19 16:04:23 +0100

    sign up

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_supabaseURL
 const supabaseKEY = import.meta.env.VITE_supabaseKEY
 
-export const supabaseClient = createClient(supabaseURL, supabaseKEY)
+export const supabase = createClient(supabaseURL, supabaseKEY)
