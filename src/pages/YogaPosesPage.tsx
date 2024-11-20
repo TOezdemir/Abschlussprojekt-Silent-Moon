956153c8b8e53a8115ed0ae2dcc56cda47c06581
Kami <kamdin@web.de>
2024-11-20 16:46:31 +0100

    yogaposes

--- src/pages/YogaPosesPage.tsx
+++ src/pages/YogaPosesPage.tsx
@@ -1,4 +1,22 @@
+import type { QueryData } from "@supabase/supabase-js"
+import { supabase } from "../lib/supabaseClient"
+
+
+
+
+
 export default function YogaPosesPage(){
+   
+    type GetYogaData = QueryData<ReturnType<typeof getAllYogaPoses>>
+    const getAllYogaPoses = async() => {
+        const result = await supabase
+        .from("yoga")
+        .select("*")
+        console.log({result});
+        return result
+    }
+    
+    getAllYogaPoses()
     return(
         <div>Yoga Poses</div>
     )
