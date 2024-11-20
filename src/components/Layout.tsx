d493183b42af0dd2f518dc60b991c9b2c7846602
Kami <kamdin@web.de>
2024-11-20 16:05:39 +0100

    frame correct

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Logo from "./Logo";
+import './layout.css'; 
 
 export default function Layout() {
   return (
-    <div className="table">
-      <div className="app-layout">
-        {/* Header */}
-        <header className="header">
-          <Logo />
-        </header>
-
-        {/* Main Content */}
-        <main className="content">
-          <Outlet />
-        </main>
-
-        <footer className="footer">
-          {/* Navbar am unteren Rand */}
-          <nav className="navbar-bottom">
-            <Navbar />
-          </nav>
-        </footer>
+    <div className="layout">
+      <div className="phone-frame">
+        <div className="screen">
+          <header className="header">
+            <Logo />
+          </header>
+          <main className="content">
+            <Outlet />
+          </main>
+          <footer className="footer">
+            <nav className="navbar-bottom">
+              <Navbar />
+            </nav>
+          </footer>
+        </div>
       </div>
     </div>
   );
