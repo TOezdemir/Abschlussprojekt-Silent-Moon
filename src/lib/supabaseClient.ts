a2e652e90c951014474c5681a7289113bb5f9329
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-11-21 09:31:17 +0100

    fetching fixed, types can be derived

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../types/supabase";
 
 const supabaseURL = import.meta.env.VITE_supabaseURL
 const supabaseKEY = import.meta.env.VITE_supabaseKEY
 
-export const supabase = createClient(supabaseURL, supabaseKEY)
+if(!supabaseURL || !supabaseKEY){
+    throw new Error(
+        "Please provide either URL or KEY!"
+    )
+}
+
+export const supabase = createClient<Database>(supabaseURL, supabaseKEY)
--- src/pages/YogaPosesPage.tsx
+++ src/pages/YogaPosesPage.tsx
@@ -2,44 +2,23 @@ import { useEffect, useState } from "react"
 import { supabase } from "../lib/supabaseClient"
 import type { QueryData } from "@supabase/supabase-js"
 
-// const getAllYogaPoses = async() => {
-//         const result = await supabase.from("yoga").select("*")
-//         console.log({result});
-//         return result
-//     }
-
-// type GetYogaData = QueryData<ReturnType<typeof getAllYogaPoses>>
-
-const getAllYogaPoses = async () => {
-    const { data, error } = await supabase.from("yoga").select("*");
-  
-    if (error) {
-      console.error("Fehler beim Abrufen der Yoga-Posen:", error);
-      return [];
+const getAllYogaPoses = async() => {
+        const result = await supabase.from("yoga").select("*")
+        console.log({result});
+        return result
     }
-  
-    return data ?? [];
-  };
+
+type GetYogaData = QueryData<ReturnType<typeof getAllYogaPoses>>
 
 export default function YogaPosesPage(){
-    // const [yoga, setYoga] = useState<GetYogaData>([])
-    const [yoga, setYoga] = useState<any[]>([])
+    const [yoga, setYoga] = useState<GetYogaData>([])
 
-    // useEffect(() =>{
-    //     getAllYogaPoses().then((result) =>{
-    //         setYoga(result)
-    //     })
-    // },[])
+    useEffect(() =>{
+        getAllYogaPoses().then((result) =>{
+            setYoga(result?.data ?? [])
+        })
+    },[])
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const yogaData = await getAllYogaPoses();
-          console.log(yogaData)
-          setYoga(yogaData);
-        };
-    
-        fetchData();
-      }, []);
     return(
         <div>
             Yoga Poses
