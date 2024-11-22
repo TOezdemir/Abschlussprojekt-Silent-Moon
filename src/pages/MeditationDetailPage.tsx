e8c482a5c13e5744325d2c76b731373fd24d5163
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-11-22 14:54:37 +0100

    overview and detailpages including favorites done

--- src/pages/MeditationDetailPage.tsx
+++ src/pages/MeditationDetailPage.tsx
@@ -1,5 +1,99 @@
-export default function MeditationDetailPage(){
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { supabase } from "../lib/supabaseClient"
+import { Link, useParams } from "react-router-dom"
+
+interface MeditaionDetailProps {
+    meditation: {
+        audio_url: string | null;
+        category_id: string | null;
+        created_at: string;
+        description: string;
+        duration: number | null;
+        id: string;
+        image_url: string | null;
+        name: string;
+        video_url: string | null;
+        meditation_categories: {
+            id: string
+            name: string
+            description: string
+        } | null;
+    }
+}
+
+
+export default function MeditationDetailPage({meditation}: MeditaionDetailProps){
+    const { id } = useParams()
+
+    const queryClient = useQueryClient()
+
+    const singleMeditationQuery = useQuery({
+        queryKey: ["supabase", "meditation", id],
+        queryFn: async () =>{
+            if(!id){
+                return null
+            }
+        const result = await supabase
+        .from("meditation")
+        .select(`
+            *,
+            meditation_categories(
+            name),
+            favorites(
+            id
+            )
+            `)
+        .eq("id", id)
+        .single()
+          if(result.error){
+              throw result.error
+          }
+          return result.data
+        },
+      })
+
+    if(singleMeditationQuery.isPending){
+        return "...loading Meditation"
+    }
+    if(singleMeditationQuery.isError || !singleMeditationQuery.data){
+        return "...can't fetch Meditation!"
+    }
+
+    const handleFavoriteClick = async () => {
+        if(!id){
+            console.error("No ID found!")
+            return
+        }
+        if(meditationTechnique.favorites.length > 0){
+            await supabase.from("favorites").delete().eq("meditation_id", id)
+        } else {
+            await supabase.from("favorites").insert({meditation_id: id})
+        }
+        queryClient.invalidateQueries({queryKey: ["supabase", "meditation"]})
+    }
+
+
+    const meditationTechnique = singleMeditationQuery.data
+
     return(
-        <div>Meditations Detail</div>
+        <div>
+            <div key={meditation.id}>
+                <div>
+                    <Link to={"/meditation"}>Zurück Pfeil</Link>
+                    <button onClick={handleFavoriteClick}>
+                        {meditationTechnique.favorites.length > 0 ? "❤️" : "♡"}</button>
+                </div>
+                <div>
+                    <img src={meditation.image_url!} alt="meditation_cover" /> 
+                </div>
+                <div>
+                    <h1>{meditation.name}</h1>
+                    <p>{meditation.meditation_categories?.name}</p>
+                    <p>{meditation.description}</p>
+                    <p>Designey Elemente? Hier wären Favorites und Listenings aber das ist quatsch. </p>
+                    <p>Hier ein Player mit hinterlegter Audiodatei</p>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/YogaPage.tsx
+++ src/pages/YogaPage.tsx
@@ -1,25 +1,105 @@
+import { useQuery } from "@tanstack/react-query";
+import { ElementRef, useRef, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import slugify from "slugify";
 import { Link } from "react-router-dom";
 
 export default function YogaPage() {
+  const [searchText, setSearchText] = useState("")
+  const inputRef = useRef<ElementRef<"input">>(null)
+
+  const allYogaQuery = useQuery({
+    queryKey: ["supabase", "yoga", searchText],
+    queryFn: async () =>{
+      const result = await supabase
+      .from("yoga")
+      .select("*")
+      .ilike("name", `%${searchText}%`)
+
+        if(result.error){
+          throw result.error
+        }
+        return result.data
+    },
+  })
+
+  if(allYogaQuery.isPending){
+    return "...loading Yoga"
+  }
+  if(allYogaQuery.isError || !allYogaQuery.data){
+    return "...can't fetch Yoga data!"
+  }
+
+  const handleSearch: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    const value = inputRef.current?.value || ""
+    setSearchText(value)
+    console.log(searchText)
+  }
+
+  const handleReset = () => {
+    inputRef.current!.value = "";
+    setSearchText("");
+  };
+
+  const allYogaPoses = allYogaQuery.data
+
   return (
     <div>
-      <h1>Yoga Page</h1>
-      
       <div>
-      <button>
-          <Link to="/yogavideo">Yoga Video</Link>
-        </button>
-        <button>
-          <Link to="/yogabinaural">Yoga Binaural</Link>
-        </button>
-        <button>
-          <Link to="/yogamantra">Yoga Mantra</Link>
-        </button>
-        <button>
-          <Link to="/yogapiano">Yoga Piano</Link>
-        </button>
-      
+        <h1>Yoga</h1>
+        <p>Find your inner zen from anywhere.</p> 
+      </div>
+      <div>
+        <div>Hier stehen später die Rubrikenbuttons</div>
+      </div>
+      <div>
+        <form onSubmit={handleSearch}>
+          <input ref={inputRef} type="text" />
+          {searchText && <button onClick={handleReset}>X</button>}
+          <button>Lupe Icon</button>
+        </form>
+      </div>
+      <div>
+        <p>Lerne Yoga kennen / Hier ist eigentlich ein großer Element, der in der Figma ein zufälliges Yoga Video abspielt. Hier könnte stattdessen ein klickbarer Banner stehen, der zu einer Seite leitet, welche Yoga und seine Wirkung beschreibt?</p>
+      </div>
+      <div>
+      {allYogaPoses.map((allYoga) =>(
+          <Link key={allYoga.id} to={`/yoga/${slugify(allYoga.name, { lower: true })}/${allYoga.id}`}>
+            <div>
+              <img src={allYoga.image_url!} alt="yoga_bgimage" />
+              <h2>{allYoga.name}</h2>
+              <p>{allYoga.difficulty}</p>
+              <p>{allYoga.duration}</p>
+              </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
 }
+
+
+// export default function YogaPage() {
+//   return (
+//     <div>
+//       <h1>Yoga Page</h1>
+      
+//       <div>
+//       <button>
+//           <Link to="/yogavideo">Yoga Video</Link>
+//         </button>
+//         <button>
+//           <Link to="/yogabinaural">Yoga Binaural</Link>
+//         </button>
+//         <button>
+//           <Link to="/yogamantra">Yoga Mantra</Link>
+//         </button>
+//         <button>
+//           <Link to="/yogapiano">Yoga Piano</Link>
+//         </button>
+      
+//       </div>
+//     </div>
+//   );
+// }
