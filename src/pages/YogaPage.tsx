cded5de74fd3ba94ee980cf384c6bc50fe6ef575
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-11-26 13:15:24 +0100

    searchbar and logout added

--- src/pages/YogaPage.tsx
+++ src/pages/YogaPage.tsx
@@ -94,27 +94,3 @@ export default function YogaPage() {
     </div>
   );
 }
-
-// export default function YogaPage() {
-//   return (
-//     <div>
-//       <h1>Yoga Page</h1>
-
-//       <div>
-//       <button>
-//           <Link to="/yogavideo">Yoga Video</Link>
-//         </button>
-//         <button>
-//           <Link to="/yogabinaural">Yoga Binaural</Link>
-//         </button>
-//         <button>
-//           <Link to="/yogamantra">Yoga Mantra</Link>
-//         </button>
-//         <button>
-//           <Link to="/yogapiano">Yoga Piano</Link>
-//         </button>
-
-//       </div>
-//     </div>
-//   );
-// }
